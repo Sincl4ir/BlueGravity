4d37d20d853eb2e553b9f4de8d4410c9641cffdc
Leandro Lopez <llopez.unitydev@gmail.com>
2023-05-25 18:14:48 -0300

    Added Tapestry module to handle static events.

---
+++ Assets/Scripts/Tapestry/TapestryEventRegistry.cs
@@ -0,0 +1,33 @@
+﻿namespace BlueGravity.Tapestry
+{
+    public class TapestryEventRegistry
+    {
+
+        #region Player
+        /// <summary>
+        /// Reports Player death
+        /// </summary>
+        //public static TapestryEvent<Entity> OnPlayerDeathTE;
+        #endregion
+      
+        static TapestryEventRegistry()
+        {
+            CreateTapestryEvents();
+        }
+
+        private static void CreateTapestryEvents()
+        {
+            #region Player
+            //OnPlayerDeathTE = new TapestryEvent<Entity>();
+            #endregion
+
+        }
+
+        public static void OnDestroy()
+        {
+            // Creates new events to clear the old ones
+            CreateTapestryEvents();
+        }
+    }
+}
+//EOF.
